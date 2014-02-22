d8480e52d613f2cbb678710ec79ae79df0d99e75
Clint Newsom <hcnewsom@gmail.com>
2014-02-21 20:00:34 -0500

    Removing directories.

--- out/browser/pong/core.js
+++
@@ -1,4 +0,0 @@
-goog.provide('pong.core');
-goog.require('cljs.core');
-console.log(cljs.core._PLUS_,1,2,3);
-console.log(cljs.core._PLUS_,6,2,3);
--- src/cljs_pong/core.clj
+++
@@ -1,6 +0,0 @@
-(ns cljs-pong.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
